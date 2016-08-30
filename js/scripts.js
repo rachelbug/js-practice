21ba3f0a6c75a672c075dc1d53bf1d11d98bb4f3
Rachel Etnire and Tyler Ferrin <tyler@>
2016-08-29 20:37:26 -0700

    add functions to calculate BMI using Javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,29 @@ var add = function(number1, number2) {
 	return number1 + number2;
 };
 
-var number1 = parseInt(prompt("enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
 
-alert(add(number1,number2));
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var bodyMassIndex = function(weight, height) {
+  return ( weight / (height * height) ) * 703;
+}
+
+var height = prompt("How tall are you in inches?");
+var weight = prompt("How much do you weigh in pounds? be honest!");
+
+// var number1 = parseInt(prompt("enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+
+
+alert(bodyMassIndex(weight, height));
+
+alert(subtract(number1,number2));
